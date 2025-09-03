f07f0bf63e62e232f3daa40d46b9fc93e9b225f0
harunoSak200 <kshatriyaprojectsgithubs@gmail.com>
2025-09-03 20:54:37 +0530

    pushed

--- cohort0_1/React JS/prop_drilling_in_react/src/App.jsx
+++ cohort0_1/React JS/prop_drilling_in_react/src/App.jsx
@@ -1,16 +1,115 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState , useContext } from 'react'
+import { CountContext } from './Context'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <CountContext.Provider value={{count , setCount}}>
+        <Count/>  
+      </CountContext.Provider>
     </>
   )
 }
 
+function Count(){
+  return (
+    <div>
+      <RenderCount></RenderCount>
+      <Buttons/>
+    </div>
+  )
+}
+function RenderCount(){
+  const {count} = useContext(CountContext)
+  return (
+    <div>
+      <h1>{count}</h1>
+    </div>
+  )
+}
+function Buttons(){
+  const {count , setCount}= useContext(CountContext)
+  return (
+    <div>
+
+      <button onClick={()=>{
+        setCount(count+1); 
+      }} style={{
+          padding:15 , 
+          background:"black" ,
+          color : "yellow"
+      }}><strong>+</strong></button>
+
+      <button onClick={()=>{
+        setCount(count-1)
+      }} style={{
+        padding : 15,
+        background:"#fa46af" , 
+        color:"blue"
+      }}><strong>-</strong></button>
+
+    </div>
+  )
+}
+
 export default App
+
+
+// This code is the example of the prop-drilling which makes the code non-manageable , 
+// Ths is code is without the use of the contextApi 
+
+// import { useState } from 'react'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <Count count={count} setCount={setCount}/>
+//     </>
+//   )
+// }
+
+// function Count({count , setCount}){
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+//       <Buttons count={count} setCount={setCount}/>
+//       <RenderCount count={count}></RenderCount>
+//     </div>
+//   )
+// }
+// function RenderCount({count}){
+//   return (
+//     <div>
+//       {count}
+//     </div>
+//   )
+// }
+// function Buttons({count , setCount}){
+//   return (
+//     <div>
+
+//       <button onClick={()=>{
+//         setCount(count+1); 
+//       }} style={{
+//           padding:15 , 
+//           background:"black" ,
+//           color : "yellow"
+//       }}><strong>+</strong></button>
+
+//       <button onClick={()=>{
+//         setCount(count-1)
+//       }} style={{
+//         padding : 15,
+//         background:"#fa46af" , 
+//         color:"blue"
+//       }}><strong>-</strong></button>
+
+//     </div>
+//   )
+// }
+
+// export default App
